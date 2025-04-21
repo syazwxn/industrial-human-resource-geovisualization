153063580c1cd147c54beece3fa31a0fc63a5a4b
syazwxn <syazwannazim1511@gmail.com>
2025-04-21 13:36:58 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,20 +1,16 @@
 import pandas as pd
 
-df = pd.read_csv("classified_by_sector.csv")
-
-df = pd.read_csv("classified_by_sector.csv")  
+df = pd.read_csv(r"C:\Task\industrial-human-resource-geovisualization/classified_by_sector.csv")
 
 df.columns = df.columns.str.lower().str.strip().str.replace(' ', '_')
 df.rename(columns={'india/states': 'state'}, inplace=True)
 
 df['nic_name'] = df['nic_name'].astype(str).str.lower().str.strip()
 
-
 def classify_sector(nic_text):
     if any(keyword in nic_text for keyword in [
         'agriculture', 'farming', 'crop', 'poultry', 'growing',
-        'planting', 'harvesting', 'cultivation', 'vegetables', 'rice',
-        'cereals', 'animal', 'propagation', 'raising', 'livestock', 'buffalo', 'cattle', 'sheep', 'goats', 'swine', 'pigs']):
+        'planting', 'harvesting', 'cultivation', 'vegetables', 'rice', 'cereals', 'animal']):
         return 'Agriculture'
     elif any(keyword in nic_text for keyword in ['manufacture', 'factory', 'production']):
         return 'Manufacturing'
@@ -41,4 +37,4 @@ print("\nSector Distribution:")
 print(df['sector'].value_counts())
 
 df.to_csv(r"C:\Task\classified_by_sector.csv", index=False)
-df.to_csv(r"C:\Task\classified_by_sector.csv", index=False)
+
